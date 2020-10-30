d2285ba829dfb90bbff1214a169c0b0e6c19225e
Renan <renan.baialuna@gmail.com>
2020-10-29 21:21:17 -0300

    Initial Commit

---
+++ Hacker News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hacker News
+//
+//  Created by Renan Baialuna on 29/10/20.
+//  Copyright © 2020 Renan Baialuna. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
